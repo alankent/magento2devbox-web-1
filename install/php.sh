2693b224de7ae2d9bf7f65ad78b9103f3025ab25
Alan Kent <akent@magento.com>
2017-08-29 12:18:02 -0700

    Added some extra PHP libraries.

--- install/php.sh
+++ install/php.sh
@@ -9,7 +9,7 @@ set -x
 export PHP_EXTRA_CONFIGURE_ARGS="--enable-fpm --with-fpm-user=magento2 --with-fpm-group=magento2"
 docker-php-ext-configure gd --with-freetype-dir=/usr/include/ --with-jpeg-dir=/usr/include/
 docker-php-ext-configure hash --with-mhash
-docker-php-ext-install -j$(nproc) mcrypt intl xsl gd zip pdo_mysql opcache soap bcmath json iconv
+docker-php-ext-install -j$(nproc) mcrypt intl xsl gd zip pdo_mysql opcache soap bcmath json iconv dom mbstring
 curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
 pecl install xdebug
 docker-php-ext-enable xdebug
